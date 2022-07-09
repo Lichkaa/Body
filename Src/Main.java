a7b883453a7ae548c7a5e73c0e1e92d4c9190e9b
Lichkaa <105723096+Lichkaa@users.noreply.github.com>
2022-07-09 22:19:42 +0300

    Update and rename src/Main.java to Src/Main.java

--- Src/Main.java
+++ Src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int m;
-        double h;
+        int weight;
+        double height;
         double result = service.calculate(82, 1.73);
         System.out.println("Индекс массы тела равен");
         System.out.println(result);
